78ac57a7e60ebc106c951510e683c57d68c033a0
Nucci-karim <122011978+Nucci-karim@users.noreply.github.com>
2023-06-28 17:05:45 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import axios from 'axios';
+import {store} from './store';
+
+export default{
+  name: 'app',
+  components:{
+    
+  },
+  data(){
+    return{
+      store
+    }
+  },
+  created(){
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <div id="tutto">
+    
+  </div>
+  
+  
+</template>
+
+<style lang="scss">
+@use './style/main.scss' as *;
+
+#tutto{
+height: 100vh;
+}
+
+
+</style>
